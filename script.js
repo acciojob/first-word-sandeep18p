46e73b8c4da2477ccae2c65b891a498b4991af29
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-19 21:53:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstWord(s) {
-  // your code here
+    let startIndex = 0;
+
+    while (startIndex < str.length && str[startIndex] === ' ') {
+        startIndex++;
+    }
+
+    let endIndex = startIndex;
+
+    while (endIndex < str.length && str[endIndex] !== ' ') {
+        endIndex++;
+    } 
+
+    return str.substring(startIndex, endIndex);
+
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
